a6a80d894e86e1743810a54afe40b8d69ab7fee8
Shyaboi <ian.shane.sears@gmail.com>
2020-08-29 21:13:59 -0700

    made routes dynamic and shortened params for n.pop()

--- index.js
+++ index.js
@@ -1,40 +1,65 @@
+const routeArray = [
+  route1="/",
+  route2="/index",
+  route3="/dexy",
+  route4="/wildcard"
+];
+
 // Main class exported with extra  legacy functions
+
+
 class Pop {
-    
-
+  
+  
   hello() {
-      console.log('Hello npm friends');
+    console.log('Hello npm friends');
   }
-
+  
   nThing(){
     console.log(pop);
-
   }
-
-  dexyJson (req, res, next) {
+  
+  dexyGet = (req, res, next) => {
     res.json([{ Expample: 'This is some data that could be resed by nPoP if you set the server GET settings.' }, {anoter1:"this is more data"},
-  {Another: "Dexy has a POST route as well ( ͡° ͜ʖ ͡°)"}]);
+    {Another: "Dexy has a POST route as well ( ͡° ͜ʖ ͡°)"}]);
+  }
+  
+  homeGet = (req, res, next, dex="../../views/index.html")=>{
+    var path = require('path');
+   
+    res.sendFile(path.join(__dirname, dex));
+    }
+  indexGet = (req, res, next, index="../../views/index2.html")=>{
+      var path = require('path');
+     
+      res.sendFile(path.join(__dirname, index));
+      }
+    nuevoRoute = ()=> {}
+    
+
   }
 
-  shyaboi() {
-    console.log('Shysadasaboi!');
+
+
+getArr = ()=> {
+  app.get(route4, function (req, res, next) {
+    res.send("hiwrlsdff")
+    });
 }
-}
+
+
 module.exports = Pop
 
 
 
 class Server extends Pop {
-
-pop(
-ok = this.dexyJson,
-pN=process.env.PORT||3333,
- route1="/",
-  route2="/index",
-   route3="/dexy",
-    index="../../views/index.html",
-    dex="../../views/index2.html",
-     dexy="data might go here"
+  
+  pop(
+    homeGet = this.homeGet,
+    dexyGet = this.dexyGet,
+    indexGet = this.indexGet,
+    pN=process.env.PORT||3333,
+    ok = [routeArray],
       ){
   
   const express = require('express')
@@ -45,30 +70,26 @@ pN=process.env.PORT||3333,
   const port = pN
   
   
-  app.use("/", express.static("./views/imgs"));
-  app.use("/index", express.static("./views/imgs"));
-  app.use("/dexy", express.static("./views/imgs"));
-  app.use("/dexy", express.static("./views/js"));
-  app.use("/index", express.static("./views/js"));
-  app.use("/", express.static("./views/js"));
-  app.use("/", express.static("./views/styles"));
-  app.use("/index", express.static("./views/styles"));
-  app.use("/dexy", express.static("./views/styles"));
+  app.use(route1, express.static("./views/imgs"));
+  app.use(route1, express.static("./views/js"));
+  app.use(route1, express.static("./views/styles"));
+  app.use(route2, express.static("./views/imgs"));
+  app.use(route2, express.static("./views/js"));
+  app.use(route2, express.static("./views/styles"));
+  app.use(route3, express.static("./views/imgs"));
+  app.use(route3, express.static("./views/js"));
+  app.use(route3, express.static("./views/styles"));
   const bodyParser = require("body-parser");
   app.use(bodyParser.json()); // to support JSON bodies
   app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded 
 
   
 
-  app.get(route1, function (req, res, next) {
-res.sendFile(path.join(__dirname, index));
-});
+app.get(route1, homeGet);
 
-app.get(route2, function (req, res, next) {
-res.sendFile(path.join(__dirname, dex));
-});
+app.get(route2, indexGet);
 
-app.get(route3, ok)
+app.get(route3, dexyGet)
 
 app.post(route3, function (req, res, next) {
 const stuff = req.body
@@ -80,6 +101,6 @@ res.redirect("/")
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
 
 
-}}
-
+}
+}
 module.exports = Server
